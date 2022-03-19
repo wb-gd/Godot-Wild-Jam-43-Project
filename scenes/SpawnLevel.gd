05e1befa761eb1c613e4484088c5e97495c5cc00
wb-gd <will.brown.gd@gmail.com>
2022-03-18 22:33:09 -0400

    Intial setup for walker

---
+++ scenes/SpawnLevel.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+const DIRECTIONS = [Vector2.RIGHT, Vector2.UP, Vector2.LEFT, Vector2.DOWN]
+
+var _position = Vector2.ZERO
+var _direction = Vector2.RIGHT
+var _boarders = Rect2()
+var _stepHistory = []
+
+var _tbOpen = preload("res://walls/TB_Open.tscn")
+var _tbClosed = preload("res://walls/TB_Closed.tscn")
+var _lrOpen = preload("res://walls/LR_Open.tscn")
+var _lrClosed = preload("res://walls/LR_Closed.tscn")
+
+var rooms = []
+
+func loadRooms():
+	var path = "res://path/to/folder"
+	var dir = Directory.new()
+	dir.open(path)
+	dir.list_dir_begin()
+	while true:
+		var file_name = dir.get_next()
+		if file_name == "":
+			break
+		elif !file_name.begins_with(".") and !file_name.ends_with(".import"):
+			rooms.append(load(path + "/" + file_name))
+	dir.list_dir_end()
+
+func _ready():
+	loadRooms()
+	
